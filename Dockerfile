0d7bad8b9c1ee9e07c57842d98f1f670b41360b6
cyad22 <p.a.s.p@mail.ru>
2019-11-26 08:48:11 +0100

    test bot code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
 WORKDIR /code
-ADD sleepwellbot/main.py /code
-ADD sleepwellbot/requirements.txt /code
+ADD sleepwellbot/main.py .
+ADD sleepwellbot/requirements.txt .
 RUN pip3 install -r ./requirements.txt
 CMD ["python", "./main.py"]
--- sleepwellbot/main.py
+++ sleepwellbot/main.py
@@ -1,1 +1,13 @@
-print("botStarted")
+import telebot
+
+bot = telebot.TeleBot("989082753:AAHYNJHhBQqMqjoXAMerC0g-_BfAYziLtAY")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
