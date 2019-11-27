92bf7fcfdd80047174f6c40a73894a3d72807c1a
pavels <shishkinpi@gmail.com>
2019-11-27 23:15:43 +0300

    Log

--- sleepwellbot/main.py
+++ sleepwellbot/main.py
@@ -9,6 +9,6 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     print(message.chat.id)
-    bot.send_message(message, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 bot.polling()
