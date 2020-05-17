9499baf0d70079169166e040612e74f733a0326f
pavels <shishkinpi22@gmail.com>
2020-05-17 22:44:31 +0300

    fix

--- sleepwellbot/main.py
+++ sleepwellbot/main.py
@@ -12,30 +12,17 @@ PORT = 80
 LISTEN = '0.0.0.0'
 URL_BASE = f"https://{HOST}:{PORT}"
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
-
 bot = telebot.TeleBot(API_TOKEN)
-
-# @app.route('/', methods=['GET', 'HEAD'])
-# def webhook():
-#     data = flask.request.query_string
-#     bot.process_new_updates([data])
-#     return data
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    CHAT_ID = message.chat.id
-    bot.reply_to(message, f"Now your this chat used for alarming. Chat id: {CHAT_ID}")
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     print(message.chat.id)
-#     bot.reply_to(message.chat.id, message.text)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
 
-# bot.remove_webhook()
-
-time.sleep(3)
+bot.polling()
 
 # bot.set_webhook(url=f"{URL_BASE}/")
 
